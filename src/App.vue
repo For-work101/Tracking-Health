46d164b2a2c44a78162fb163a94aeea4feea6928
For-work101 <fornormalactivity@gmail.com>
2026-06-18 19:07:23 +0700

    color

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,24 @@ html,
 body,
 #app {
   margin: 0;
-  min-height: 100vh;
+  padding: 0;
+  min-height: 100%;
+}
+
+body {
+  background: linear-gradient(
+    to top,
+    #66bb6a 0%,
+    #81c784 25%,
+    #a5d6a7 50%,
+    #c8e6c9 75%,
+    #f1f8e9 100%
+  );
+
+  background-attachment: fixed;
 }
 
 .app-bg {
   min-height: 100vh;
-
-  background: linear-gradient(
-  to top,
-  #66bb6a 0%,
-  #81c784 25%,
-  #a5d6a7 50%,
-  #c8e6c9 75%,
-  #f1f8e9 100%
-);
 }
 </style>
